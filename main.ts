8217cbe3df75976d0720fdcdceb45d96ca07adca
nnortonschool <72471414+nnortonschool@users.noreply.github.com>
2022-09-27 13:19:15 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+        if (Math.randomBoolean()) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+            basic.pause(250)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            basic.pause(250)
+        }
     } else {
-        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     }
 })
